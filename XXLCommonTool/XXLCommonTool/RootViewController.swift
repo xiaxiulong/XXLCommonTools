12a467a09a998ae16c6adb50ed465ba6a51f3d54
LuckyLong <xiaxiulong@users.noreply.github.com>
2016-10-13 00:58:10 +0800

    update swift proj
    update swift proj

--- XXLCommonTool/XXLCommonTool/RootViewController.swift
+++ XXLCommonTool/XXLCommonTool/RootViewController.swift
@@ -10,15 +10,6 @@ import UIKit
 
 class RootViewController: UIViewController {
     var tableview:UITableView?
-
-//    init() {
-//        tableview = UITableView(frame: UIScreen.main.bounds)
-//        super.init(coder: NSCoder())!
-//    }
-//    
-//    required init?(coder aDecoder: NSCoder) {
-//        super.init(coder: aDecoder)
-//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +17,8 @@ class RootViewController: UIViewController {
         self.view.backgroundColor =  UIColor.yellow
         
         tableview = UITableView(frame: UIScreen.main.bounds)
+        tableview?.delegate = self
+        tableview?.dataSource = self
         self.view.addSubview(tableview!)
     }
     
@@ -35,5 +28,79 @@ class RootViewController: UIViewController {
     }
     
     
+}
+
+extension RootViewController: UITableViewDataSource,UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //TODO:
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell : UITableViewCell?
+        let cellIdentifier = "kNormalCell"
+        
+        cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        if cell == nil {
+            cell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: cellIdentifier)
+            cell?.accessoryType = UITableViewCellAccessoryType.disclosureIndicator
+        }
+        cell?.textLabel?.text = "test title"
+        cell?.detailTextLabel?.text = "sub title"
+        
+        return cell!
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "XXLCommonTool"
+    }
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        
+    }
+    func tableView(_ tableView: UITableView, didHighlightRowAt indexPath: IndexPath) {
+        
+    }
+    func tableView(_ tableView: UITableView, didEndEditingRowAt indexPath: IndexPath?) {
+        
+    }
+    func tableView(_ tableView: UITableView, didUnhighlightRowAt indexPath: IndexPath) {
+        
+    }
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    func tableView(_ tableView: UITableView, canFocusRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    func tableView(_ tableView: UITableView, willBeginEditingRowAt indexPath: IndexPath) {
+        
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50.0
+    }
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return nil
+    }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return nil
+    }
+    func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        return "the end!"
+    }
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 20
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 20
+    }
+    func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
 }
 
